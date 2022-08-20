698e8c4d6bf1f648eec3c5b03751d51193668afa
acer <fsaidov167@gmail.com>
2022-08-20 18:57:37 +0500

    pdpui5

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  static final String id = "home_page";
+  static final String id = "home_page ";
 
   @override
   _HomePageState createState() => _HomePageState();
