57a1b3de3add2dbbe799b3b583027f48feaee20a
acer <fsaidov167@gmail.com>
2022-08-20 18:55:14 +0500

    pdpui5

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -96,7 +96,7 @@ class _HomePageState extends State<HomePage> {
                     ),
                   ),
                   SizedBox(height: 20,),
-                  Text('Resort Hotels ',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
+                  Text('Resort Hotels',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20),),
 
                   Container(
                     height: 200,
